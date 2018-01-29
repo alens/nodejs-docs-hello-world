864f07c56c0a98fec17197c64455b90cab5fb41e
Alen Stimec <alens@users.noreply.github.com>
2018-01-29 16:48:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!, 2018-jan-29 2:05pm");
+    response.end("Hello World!, 2018-jan-29 4:50pm");
 
 });
 
